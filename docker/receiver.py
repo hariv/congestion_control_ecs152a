21b4f3179687736a2aec52336a47d7042b51f1c8
hariv <hvenugopalan@ucdavis.edu>
2023-11-29 18:45:56 -0800

    duplicate acks and packet loss

--- docker/receiver.py
+++ docker/receiver.py
@@ -3,19 +3,18 @@ import socket
 
 PACKET_SIZE = 1024
 SEQ_ID_SIZE = 4
+MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
+EXPECTED_SEQ_ID = 0
+RECEIVED_DATA = {}
 
-def create_acknowledgement(seq_id):
-    return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + b'ack'
+def create_acknowledgement(seq_id, message):
+    return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + message.encode()
 
 # create a udp socket
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     # bind the socket to a OS port
     # bind to 0.0.0.0 so external 
     udp_socket.bind(("0.0.0.0", 5001))
-
-    # file to write to
-    # write to some file on the hard disk
-    recv = open('/hdd/recv.txt', 'wb')
 
     print("Receiver running")
     # start receiving packets
@@ -24,28 +23,43 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
         try:
             # receive the packet
             packet, client = udp_socket.recvfrom(PACKET_SIZE)
-
+            
             # get the message id
             seq_id, message = packet[:SEQ_ID_SIZE], packet[SEQ_ID_SIZE:]
+            
+            # check if finack message
+            if message == b'==FINACK==':
+                break
             
             # if the message id is -1, we have received all the packets
             seq_id = int.from_bytes(seq_id, signed=True, byteorder='big')
             
-            if seq_id == -1:
-                # send the acknowledgement
-                udp_socket.sendto(create_acknowledgement(seq_id), client)
-                break
+            # keep track of received sequences
+            RECEIVED_DATA[seq_id] = message
             
-            # write message to file
-            recv.seek(seq_id)
-            recv.write(message)
-
+            # check if sequence id is same as expected and move forward
+            if seq_id <= EXPECTED_SEQ_ID and len(RECEIVED_DATA[seq_id]) > 0:
+                while EXPECTED_SEQ_ID in RECEIVED_DATA:
+                    EXPECTED_SEQ_ID += len(RECEIVED_DATA[seq_id])
+            
+            # create ack id
+            ack_id = EXPECTED_SEQ_ID
+            
             # create the acknowledgement
-            acknowledgement = create_acknowledgement(seq_id)
+            acknowledgement = create_acknowledgement(ack_id, 'ack')
 
             # send the acknowledgement
             udp_socket.sendto(acknowledgement, client)
+            
+            # check if all data received (empty message)
+            if len(message) == 0 and ack_id == seq_id:
+                ack = create_acknowledgement(ack_id, 'ack')
+                fin = create_acknowledgement(ack_id + 3, 'fin')
+                udp_socket.sendto(ack, client)
+                udp_socket.sendto(fin, client)
         except socket.timeout:
             timeouts += 1
 
-    recv.close()
+with open('/hdd/file2.mp3', 'wb') as f:
+    for sid in sorted(RECEIVED_DATA.keys()):
+        f.write(RECEIVED_DATA[sid])
--- docker/training_profile.sh
+++ docker/training_profile.sh
@@ -2,7 +2,7 @@
 
 bandwidth=100000
 
-tc qdisc add dev eth0 root handle 1:0 netem delay 100ms limit 1000000
+tc qdisc add dev eth0 root handle 1:0 netem delay 100ms reorder 5% 50% limit 1000000
 tc qdisc add dev eth0 parent 1:0 handle 2:0 htb default 1
 tc class add dev eth0 parent 2:0 classid 2:1 htb rate $bandwidth
 
